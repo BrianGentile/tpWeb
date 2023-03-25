6fca09292fc5301ef3e39292acb6455706006a34
BrianGentile <125157528+BrianGentile@users.noreply.github.com>
2023-03-25 12:57:18 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,3 @@
-
 var editingMode = { rect: 0, line: 1 };
 
 function Pencil(ctx, drawing, canvas) {
@@ -7,7 +6,6 @@ function Pencil(ctx, drawing, canvas) {
 	this.currColour = '#000000';
 	this.currentShape = 0;
 
-	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 
 	document.getElementById('butRect').onclick = () => this.currEditingMode = editingMode.rect;
 	document.getElementById('butLine').onclick = () => this.currEditingMode = editingMode.line;
@@ -22,24 +20,20 @@ function Pencil(ctx, drawing, canvas) {
 
 	new DnD(canvas, this);
 
-	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
-
 	this.onInteractionStart = function(dnd){
-		
-		if(this.currEditingMode == editingMode.rect){
-			this.currentShape = new Rectangle(dnd.initialX, dnd.initialY, this.currColour, this.currLineWidth, dnd.finalX - dnd.initialX, dnd.finalY - dnd.initialY);
 
-		}
-		else {
-		 	this.currentShape = new Line(dnd.initialX, dnd.initialY, this.currColour, this.currLineWidth, dnd.finalX, dnd.finalY);
-
-		}
-		drawing.paint(ctx);
+			if(this.currEditingMode == editingMode.rect){
+				this.currentShape = new Rectangle();
+			}
+			else{
+				this.currentShape = new Line();
+			}
 
 
 	}.bind(this);
 
 	this.onInteractionUpdate = function(dnd){
+		
 		if(this.currEditingMode == editingMode.rect){
 			this.currentShape = new Rectangle(dnd.initialX, dnd.initialY, this.currColour, this.currLineWidth, dnd.finalX - dnd.initialX, dnd.finalY - dnd.initialY);
 
@@ -54,21 +48,37 @@ function Pencil(ctx, drawing, canvas) {
 	}.bind(this);
 
 	this.onInteractionEnd = function(dnd){
-		if(this.currEditingMode == editingMode.rect){
-			this.currentShape = new Rectangle(dnd.initialX, dnd.initialY, this.currColour, this.currLineWidth, dnd.finalX - dnd.initialX, dnd.finalY - dnd.initialY);
 
-		}
-		else {
-		 	this.currentShape = new Line(dnd.initialX, dnd.initialY, this.currColour, this.currLineWidth, dnd.finalX, dnd.finalY);
-
-		}
-		drawing.Shapes.push(this.currentShape);
-		
-		drawing.paint(ctx);
+		var uid = generateUUID();
+		console.log(uid)
+		drawing.Shapes.set(uid,this.currentShape);
+		drawing.paint(ctx,canvas);
+		updateShapeList(uid,this.currentShape);
+		document.getElementById("remove" + uid).onclick = (event) => remove(drawing, event.currentTarget.id.substring(6), ctx, canvas);
 		
 	}.bind(this);
 
-
 };
+	//fonction prise sur Internet
+	function generateUUID() { // Public Domain/MIT
+		var d = new Date().getTime();//Timestamp
+		var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16;//random number between 0 and 16
+			if(d > 0){//Use timestamp until depleted
+				r = (d + r)%16 | 0;
+				d = Math.floor(d/16);
+			} else {//Use microseconds since page-load if supported
+				r = (d2 + r)%16 | 0;
+				d2 = Math.floor(d2/16);
+			}
+			return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+		});
+	}
 
-
+	function remove(drawing, index, ctx, canvas){
+		console.log(index);
+		drawing.Shapes.delete(index);
+		document.getElementById('liRemove' + index).remove()
+		drawing.paint(ctx, canvas);
+	}
