563b76711165028e4e9cf8546ffef5fab4842c3a
BrianGentile <125157528+BrianGentile@users.noreply.github.com>
2023-03-25 10:35:31 +0100

    Update view.js

--- view.js
+++ view.js
@@ -1,26 +1,30 @@
-
-// Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
-
-
 Rectangle.prototype.paint = function(ctx) {
-
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = this.thickness;
   ctx.beginPath();
-  ctx.rect(this.initialX, this.initialY, this.height, this.width);
-  ctx.stroke();
-};
 
-Line.prototype.paint = function(ctx) {
-  ctx.beginPath();
-  ctx.moveTo(this.initialX, this.initialY);
-  ctx.lineTo(this.finalX, this.finalY );
-  ctx.stroke();
-};
+    ctx.rect(this.initialX, this.initialY, this.height, this.width);
+    ctx.stroke();
+  };
+  
+  Line.prototype.paint = function(ctx) {
 
-Drawing.prototype.paint = function(ctx) {
-  ctx.fillStyle = '#F0F0F0'; // set canvas' background color
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  this.Shapes.forEach(function (eltDuTableau) {
-    // now fill the canvas
-    eltDuTableau.paint(ctx);
-  });
-};
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.thickness;
+    ctx.beginPath();
+    
+    ctx.moveTo(this.initialX, this.initialY);
+    ctx.lineTo(this.finalX, this.finalY );
+    ctx.stroke();
+  };
+  
+  Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.Shapes.forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+    });
+  };
+  
